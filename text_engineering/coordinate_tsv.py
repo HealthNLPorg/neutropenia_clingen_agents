0484e637681308c74be790fb976ced78c93d91fa
Eli Goldner <egoldner@brandeis.edu>
2024-11-29 18:34:55 -0500

    Figured out all the wacky exceptions in SDS, need to do non-space whitespace replacement on the non-sentence files (mostly diagnosis) just in case

--- text_engineering/coordinate_tsv.py
+++ text_engineering/coordinate_tsv.py
@@ -1,9 +1,10 @@
 import argparse
 import pickle
+from itertools import takewhile
 from typing import Literal
 
 import pandas as pd
-from more_itertools import flatten
+from more_itertools import flatten, partition
 from numpy import nan
 
 argparser = argparse.ArgumentParser(description="")
@@ -31,15 +32,23 @@ def complete_frame(frame_n: str, pickle_n: str, corpus: CORPUS) -> pd.DataFrame:
     def get_pdf_process(row: pd.Series) -> str:
         return row.filename.split("_")[0]
 
-    def get_mrn(row: pd.Series) -> str:
-        return row.filename.split("_")[1].split["-"][0]
+    def get_mrn(row: pd.Series) -> int | str:
+        _str_mrn = "".join(takewhile(str.isnumeric, row.filename.split("_")[1]))
+        if len(_str_mrn) > 0:
+            mrn = int(_str_mrn)
+            if mrn == 7107993:
+                return 71079936  # not terribly impressed with the flukes in this retrieval system
+            return mrn
+        # bad naming for the one patient in SDS corpus who doesn't have a BCH MRN,
+        # there are no patients with this issue in SCNIR
+        return next(mrn for mrn in MRN_to_PT.keys() if type(mrn) is str)
 
     def get_original_filename(row: pd.Series) -> str:
         return "_".join(row.filename.split("_")[1:])
 
-    frame["PDF_PROCESS"] = frame.apply(get_pdf_process)
-    frame["MRN"] = frame.apply(get_mrn)
-    frame["filename"] = frame.apply(get_original_filename)
+    frame["PDF_PROCESS"] = frame.apply(get_pdf_process, axis=1)
+    frame["MRN"] = frame.apply(get_mrn, axis=1)
+    frame["filename"] = frame.apply(get_original_filename, axis=1)
     frame["CORPUS"] = corpus
 
     for pt_column in pt_columns:
@@ -47,10 +56,16 @@ def complete_frame(frame_n: str, pickle_n: str, corpus: CORPUS) -> pd.DataFrame:
         def get_column_attr(row: pd.Series) -> str | float | int | pd.Timestamp:
             PT_info = MRN_to_PT.get(row.MRN)
             if PT_info is None:
-                raise Exception(f"Missing Table Entry for {row.MRN}")
+                numeric_mrns, string_mrns = partition(
+                    lambda s: type(s) is int, MRN_to_PT.keys()
+                )
+                mrns = [*sorted(string_mrns), *sorted(numeric_mrns)]
+                raise Exception(
+                    f"Missing Table Entry for mrn {row.MRN} from filename {row.filename} in:\n\n{mrns}"
+                )
             return PT_info.get(pt_column, nan)
 
-        frame[pt_column] = frame.apply(get_column_attr)
+        frame[pt_column] = frame.apply(get_column_attr, axis=1)
     return frame
 
 
